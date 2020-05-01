395d6dc51792a61582c7ed79d27b1e620b6a14b4
Prateek Pandey <46824331+pprockys@users.noreply.github.com>
2020-05-01 21:58:46 +0530

    Create Day_1_First_Bad_Version.cpp

---
+++ solutions/Day_1_First_Bad_Version.cpp
@@ -0,0 +1,22 @@
+//pprockys
+
+
+//problem_link---> https://leetcode.com/problems/first-bad-version/
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int l=1,r=n;
+        while(l<=r)
+        {
+            int m=l+(r-l)/2;
+            if(isBadVersion(m))
+            r=m-1;
+            else
+            l=m+1;
+        }
+        return l;
+    }
+};
