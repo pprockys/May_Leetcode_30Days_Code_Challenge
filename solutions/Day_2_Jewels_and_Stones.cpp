bd006168bce5fbc5891ea846229d6b4864ab627d
Prateek Pandey <46824331+pprockys@users.noreply.github.com>
2020-05-02 13:26:09 +0530

    Create Day_2_Jewels_and_Stones.cpp

---
+++ solutions/Day_2_Jewels_and_Stones.cpp
@@ -0,0 +1,16 @@
+//pprockys
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        set<char>s;
+        for(int i=0;i<J.size();i++)
+            s.insert(J[i]);
+        int ans=0;
+        for(int i=0;i<S.size();i++)
+        {
+            if(s.find(S[i])!=s.end())
+                ans++;
+        }
+        return ans;
+    }
+};
