e0aef2ed2a37773520a3bf29c9d915c374bfbf23
Prateek Pandey <46824331+pprockys@users.noreply.github.com>
2020-05-06 16:40:23 +0530

    Create Day_6_Majority_Element.cpp

---
+++ solutions/Day_6_Majority_Element.cpp
@@ -0,0 +1,22 @@
+//pprockys
+class Solution {
+    
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0,ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(count==0)
+            {
+                ans=nums[i];
+                count++;
+            }
+            else
+            {
+                if(nums[i]==ans)count++;
+                else count--;
+            }
+        }
+        return ans;
+    }
+};
